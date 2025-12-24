af33b4f49b89d29b70d65bdaa89704372eb20da8
Mohammed Shahal <78613952+mohammedshahalatholi@users.noreply.github.com>
2025-12-24 16:31:36 +0000

    worked on genrator function

---
+++ grnrator.py
@@ -0,0 +1,29 @@
+import time
+
+def comment(num):
+    def dec():
+        print("program started")
+        stated=time.time()
+        num()
+        ended=time.time()
+
+        print("program ended")
+        print("Programme ended within",ended-stated)
+    return dec
+
+def num():
+    print("one")
+    time.sleep(1)
+#without using @ key
+nums=comment(num)
+nums()
+
+#with using decorator
+@comment
+def num2():
+    print("Two")
+    time.sleep(1)
+
+num2()
+
+
